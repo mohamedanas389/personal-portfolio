03522c98d95f04f2986d9299b7239473ab77e1a9
mohamedanas389 <anashaja21to23a1cs@gmail.com>
2026-09-08 14:53:47 +0530

    portfolio updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { FaBars, FaTimes, FaGithub, FaLinkedin, FaTwitter, FaInstagram, FaEnvelo
 import { Link } from 'react-scroll';
 import anas from './assets/anas.png';
 import resume from './assets/mohamedanasresume.pdf';
-import student from './assets/ss.jpg';
-import placement from './assets/pp.jpg';
-import quiz from './assets/quiz.jpg';
+import ss from './assets/ss.jpg';
+import pp from './assets/pp.jpg';
+import quizz from './assets/quiz.jpg';
 
 
 // --- Data ---
@@ -19,19 +19,19 @@ const projects = [
     title: "Online Quiz Management System",
     desc: "A comprehensive platform to conduct quizzes and analyze results efficiently. Features include timer, instant feedback, and score analytics.",
     tech: ["MongoDB", "Node.js","Express.js","React.js"],
-    imag: quiz
+    image: quizz
   },
   {
     title: "Student Task Management System",
     desc: "Designed to track student progress efficiently. Allows task assignment, status updates, and performance tracking for educators.",
     tech: ["React.js", "Node.js","Express.js", "MongoDB"],
-    imag: student
+    image: ss
   },
   {
     title: "Placement Preparation App",
     desc: "An all-in-one app to help students prepare for placements. Includes resources, mock tests, and company-specific preparation guides.",
     tech: ["React.js", "Node.js", "Express.js","MongoDB"],
-    imag: placement
+    image: pp
   }
 ];
 
@@ -162,7 +162,7 @@ const Projects = () => {
           {projects.map((project, index) => (
             <div key={index} className="project-card">
               <div className="project-img">
-                <img src={project.imag} alt={project.title} />
+                <img src={project.image} alt={project.title} />
               </div>
               <div className="project-content">
                 <h3 className="project-title">{project.title}</h3>
